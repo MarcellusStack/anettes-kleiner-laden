f8d74608b633bef69086641007a4b2b0214aae19
MarcellusStack <Marcellusstack@gmail.com>
2025-05-04 19:13:04 +0200

    feat: add Marquee component for animated text display on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { Navbar } from "./_components/navbar";
 import { Header } from "./_components/header";
 import { ProductSequence } from "./_components/product-sequence";
+import { Marquee } from "./_components/marquee";
 
 export default function Home() {
   return (
@@ -10,6 +11,11 @@ export default function Home() {
       <Header />
       <div className="h-screen" />
       <ProductSequence />
+      <div className="h-screen"></div>
+      <Marquee
+        text1="Hello World lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+        text2="Hello World lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+      />
       <div className="h-screen"></div>
     </>
   );
